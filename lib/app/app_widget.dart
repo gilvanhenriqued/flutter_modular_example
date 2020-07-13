b4947f45dde11dbf96f5bdad09dbb964d1d79906
Gilvan Henrique <gilvanh.15@gmail.com>
2020-07-13 10:28:23 -0300

    add flutter_modular and initialroute

---
+++ lib/app/app_widget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      initialRoute: '/',
+    );
+  }
+}
