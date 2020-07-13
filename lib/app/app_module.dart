75856bb3d9b47ebdc0ca757f4f2648e4fcd65eb4
Gilvan Henrique <gilvanh.15@gmail.com>
2020-07-13 10:45:34 -0300

    add home module and controllers on modular binds

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,12 +1,14 @@
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_modular_example/app/app_controller.dart';
 import 'package:flutter_modular_example/app/app_widget.dart';
+import 'package:flutter_modular_example/app/views/home/home_view.dart';
 
 class AppModule extends MainModule {
   @override
   List<Bind> get binds => [
-
+    Bind((i) => AppController()),
+    Bind((i) => HomeController()),
   ];
 
   @override
---
+++ lib/app/views/home/home_view.dart
@@ -0,0 +1,8 @@
+import 'package:mobx/mobx.dart';
+part 'home_view.g.dart';
+
+class HomeController = _HomeControllerBase with _$HomeController;
+
+abstract class _HomeControllerBase with Store {
+  
+}
